aa37717ddeb8e524905da5e369cb42149c12fc91
Gentletouch1 <adeyemishobukola@gmail.com>
2023-09-24 06:11:26 -0700

    user.py

--- models/city.py
+++ models/city.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-"""Defines the City class."""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import ForeignKey
-from sqlalchemy import String
+"""This is the user class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 
-class City(BaseModel, Base):
-    """Represents a city for a MySQL database.
-
-    Inherits from SQLAlchemy Base and links to the MySQL table cities.
-
+class User(BaseModel, Base):
+    """This is the class for user
     Attributes:
-        __tablename__ (str): The name of the MySQL table to store Cities.
-        name (sqlalchemy String): The name of the City.
-        state_id (sqlalchemy String): The state id of the City.
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
     """
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    places = relationship("Place", backref="cities", cascade="delete")
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
